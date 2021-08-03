99558e8bc5431d1a2e53e54bf14990003299b732
Edvinas Stanys <79093402+EStanys@users.noreply.github.com>
2021-08-03 15:07:16 +0300

    Update requests.js

--- frontend/src/utils/requests.js
+++ frontend/src/utils/requests.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const URL = 'http://localhost:4000/api/users'
+const URL = `http://localhost:${process.env.PORT || 4000}/api/users`
 
 const createNewUser = async (newUserData) => {
   try {
